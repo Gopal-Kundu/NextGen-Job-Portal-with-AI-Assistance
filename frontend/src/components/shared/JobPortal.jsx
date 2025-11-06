2c3d0724e6cc73570180e517beb7d8964d42f6d5
Gopal-Kundu <kundugopal2005@gmail.com>
2025-11-06 07:53:18 +0530

    UI Chnages

--- frontend/src/components/shared/JobPortal.jsx
+++ frontend/src/components/shared/JobPortal.jsx
@@ -5,7 +5,7 @@ function JobPortal() {
       <>
         <h1 className="text-2xl md:text-4xl font-bold text-gray-800 select-none">
           <div>
-            Job<span className="text-purple-600">Portal</span>
+            Job <span className="text-purple-600">Portal</span>
           </div>
         </h1>
       </>
