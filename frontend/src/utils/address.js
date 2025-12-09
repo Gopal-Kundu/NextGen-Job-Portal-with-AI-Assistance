fc044ad9122c17a8c6ca67655370e48cab00184f
Gopal-Kundu <kundugopal2005@gmail.com>
2025-12-09 06:54:44 +0530

    Address.js modified

--- frontend/src/utils/address.js
+++ frontend/src/utils/address.js
@@ -1,5 +1,5 @@
 const USER = "http://localhost:3001";
-
+// const USER = "https://job-portal-red-two.vercel.app";
 export const USER_API_END_POINT = `${USER}/api/v1/user`
 export const JOB_API_END_POINT = `${USER}/api/v1/job`
 export const COMPANY_API_END_POINT = `${USER}/api/v1/company`
