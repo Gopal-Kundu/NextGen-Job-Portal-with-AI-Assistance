5e17f6595e3433d887b6c26136e4fd688ef168d3
Gopal-Kundu <kundugopal2005@gmail.com>
2025-12-17 08:27:31 +0530

    added pagination feature

--- frontend/src/components/ui/PaginationRounded.jsx
+++ frontend/src/components/ui/PaginationRounded.jsx
@@ -4,27 +4,21 @@ import { JOB_API_END_POINT } from "@/utils/address";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import axios from "axios";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function PaginationRounded() {
   let totalJobs = useSelector((state) => state.job.totalJobs);
   let filter = useSelector((state) => state.job.filter);
   let isFilterApplied = useSelector((state) => state.job.filterApplied);
-
   const dispatch = useDispatch();
   totalJobs = Math.ceil(totalJobs / 6);
-
   const [page, setPageNo] = useState(1);
   const [defaultPage, setDefaultPage] = useState(1);
-
-  const topRef = useRef(null);
-
   useEffect(() => {
     async function getJobs() {
       try {
         dispatch(setLoading(true));
-
         if (isFilterApplied) {
           setDefaultPage(1);
           setPageNo(1);
@@ -32,7 +26,6 @@ export default function PaginationRounded() {
         } else {
           setDefaultPage(page);
         }
-
         const res = await axios.post(
           `${JOB_API_END_POINT}/filter/${page}`,
           filter
@@ -41,31 +34,28 @@ export default function PaginationRounded() {
         dispatch(setTotalJobs(res.data.countJobs));
         dispatch(setJobs(res.data.jobs));
       } catch (error) {
-        console.error(error);
       } finally {
         dispatch(setLoading(false));
       }
     }
 
-    getJobs();
+    getJobs(page);
   }, [page, filter]);
 
-  useEffect(() => {
-    if (topRef.current) {
-      topRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  }, [page]);
-
   return (
-    <div ref={topRef}>
-      <Stack spacing={2}>
-        <Pagination
-          count={totalJobs}
-          shape="rounded"
-          page={defaultPage}
-          onChange={(e, pageno) => setPageNo(pageno)}
-        />
-      </Stack>
-    </div>
+    <Stack spacing={2}>
+      <Pagination
+        count={totalJobs}
+        shape="rounded"
+        page={defaultPage}
+        onChange={(e, pageno) => {
+          setPageNo(pageno);
+          window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+          });
+        }}
+      />
+    </Stack>
   );
 }
